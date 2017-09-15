252f754ce531d962837d35932bcb6df81108efd7
ZGLindo <Zaki.Lindo@gmail.com>
2017-09-15 05:34:54 -0400

    Final version with comments

--- p001/Printascii.s
+++ p001/Printascii.s
@@ -18,7 +18,7 @@
 base:                         // the base address of the data segment
 // the loop control variable
 x:      .word 0
-// format string for the output. %c - print assigned ascii char for val
+// format string for the output. %c - print assigned ascii char value
 str:    .asciz "%c "
 
 // assembly constants for addresses to the variables in the data seg
@@ -34,17 +34,20 @@ str:    .asciz "%c "
  *
  * r9 is set to the address of base, permitting access to the
  * variables in the .data segment with a base-offset notation. Then x
- * is initialized and the while loop runs. In-line comments track the
- * C-language equivalent code as the program proceeds.
+ * is initialized to the ascii value of 0. The while loop runs, looping
+ * through each number. Then x is reinitialized as the ascii value for 'a'.
+ * It loops until it hits the value for 'z'. Finally, x reinitializes as
+ * the ascii value for 'A'. It loops until it hits the value for 'Z'.
+ *
  *
  * This function does not use argc or argv.
  */
 
  main:
- 		push 	        {r9, lr}
-		baseaddr .req r9
+ 		push 	        {r9, lr}       // Save r9 and lr onto the stack
+		baseaddr .req r9               // Assign an alias for r9
  		ldr		baseaddr, base_address
-                mov             r0, #48
+                mov             r0, #48        // Initialize to the start of ints
                 str             r0, [baseaddr, #offsetx]               
 
  		// while (x < 58)
@@ -53,7 +56,7 @@ str:    .asciz "%c "
 
  		//if (x >= 58)
  		cmp		r0, #58
- 		bge		lowercase
+ 		bge		lowercase      
 
  		// if (x < 58)
  		ldr 	        r0, [baseaddr, #offsetx]
@@ -63,10 +66,10 @@ str:    .asciz "%c "
  		b 		afterdigitloopprintf
 
  lowercase:
-                mov             r1, #10
+                mov             r1, #10         
                 add             r0, baseaddr, #offsetstr
-                bl              printf
- 		mov		r0, #97
+                bl              printf          // Print new line
+ 		mov		r0, #97         // Initialize 'a'
  		str 	        r0, [baseaddr, #offsetx]
 
  		// while (x < 123)
@@ -81,14 +84,14 @@ str:    .asciz "%c "
  		ldr 	        r0, [baseaddr, #offsetx]
                 mov             r1, r0
                 add             r0, baseaddr, #offsetstr
- 		bl		printf
+ 		bl		printf           // Print lowercasechar
  		b 		afterlowercaseprintf
  		// while (x < 91)
  caps:
                 mov             r1, #10
                 add             r0, baseaddr, #offsetstr
-                bl              printf
-		mov		r0, #65
+                bl              printf          // Print new line
+		mov		r0, #65         // Initialize 'A'
 		str 	        r0, [baseaddr, #offsetx]
 
 		// while (x < 91)
@@ -103,11 +106,13 @@ str:    .asciz "%c "
  		ldr 	        r0, [baseaddr, #offsetx]
                 mov             r1, r0
                 add             r0, baseaddr, #offsetstr
- 		bl		printf
+ 		bl		printf           // Print Uppercase char
  		b 		aftercapsprintf
 
  afterdigitloopprintf:
  		ldr		r0, [baseaddr, #offsetx]
+
+                // x++
  		add 	        r0, r0, #1
  		str 	        r0, [baseaddr, #offsetx]
 
@@ -115,6 +120,8 @@ str:    .asciz "%c "
 
  afterlowercaseprintf:
  		ldr		r0, [baseaddr, #offsetx]
+
+                // x++
  		add 	        r0, r0, #1
  		str 	        r0, [baseaddr, #offsetx]
 
@@ -122,6 +129,8 @@ str:    .asciz "%c "
 
  aftercapsprintf:
  		ldr		r0, [baseaddr, #offsetx]
+
+                // x++
  		add 	        r0, r0, #1
  		str 	        r0, [baseaddr, #offsetx]
 
